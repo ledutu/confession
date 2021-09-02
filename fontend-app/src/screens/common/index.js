0bf10b90ef52e0f1822d8ba23b4b40e23fba5cbc
LCoder <loidv@midota.com>
2021-09-02 11:34:12 +0700

    update

--- fontend-app/src/screens/common/index.js
+++ fontend-app/src/screens/common/index.js
@@ -1,10 +1,6 @@
 import {routes} from 'utils';
-import AddQuestion from './AddQuestion';
-import RateQuestion from './RateQuestion';
 import SettingProfile from './SettingProfile';
 
 export default {
   [routes.SETTING_PROFILE_SCREEN]: SettingProfile,
-  [routes.ADD_QUESTION_SCREEN]: AddQuestion,
-  [routes.RATE_QUESTION_SCREEN]: RateQuestion,
 };
