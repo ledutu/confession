47a9b6153fcb045ebbb158b2fc14276d07c75532
LCoder <loidv@midota.com>
2021-09-02 14:07:51 +0700

    update

--- fontend-app/src/screens/bottom/Question/index.js
+++ fontend-app/src/screens/bottom/Question/index.js
@@ -1,8 +1,20 @@
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import {Container} from 'components';
 import React from 'react';
+import AddQuestion from './screens/AddQuestion';
+import RateQuestion from './screens/RateQuestion';
+
+const Tab = createMaterialTopTabNavigator();
 
 const Question = () => {
-  return <Container title="Câu hỏi" canGoBack={false} />;
+  return (
+    <Container title="Câu hỏi" canGoBack={false}>
+      <Tab.Navigator>
+        <Tab.Screen name="AddQuestion" component={AddQuestion} />
+        <Tab.Screen name="RateQuestion" component={RateQuestion} />
+      </Tab.Navigator>
+    </Container>
+  );
 };
 
 export default Question;
