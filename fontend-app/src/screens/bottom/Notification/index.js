2bc40399873b6b8ebf4b93ae0509604f58517ed4
LCoder <loidv@midota.com>
2021-09-03 14:05:39 +0700

    update

--- fontend-app/src/screens/bottom/Notification/index.js
+++ fontend-app/src/screens/bottom/Notification/index.js
@@ -1,8 +1,46 @@
 import {Container} from 'components';
+import {Avatar, Box, FlatList, HStack, Text, VStack} from 'native-base';
 import React from 'react';
 
-const NotificationScreen = ({navigation}) => {
-  return <Container title="Thông báo" canGoBack={false} isLineBottom />;
+const NotificationScreen = () => {
+  const _renderSeparator = () => <Box height={5} />;
+  const _keyExtractor = (_, index) => String(index);
+
+  const _renderItem = ({}) => {
+    return (
+      <HStack p={3} rounded={15} space={3} bg="brand.primary">
+        <Avatar
+          size="md"
+          rounded={10}
+          borderWidth={2}
+          borderColor="brand.primaryText"
+        />
+        <VStack space={1} flex={1}>
+          <Text color="white" fontSize="md">
+            Title
+          </Text>
+          <Text color="brand.primaryText" fontSize="sm">
+            Content
+          </Text>
+        </VStack>
+        <Text color="brand.primaryText" fontSize="xs">
+          12/21/2021
+        </Text>
+      </HStack>
+    );
+  };
+
+  return (
+    <Container title="Thông báo" canGoBack={false}>
+      <FlatList
+        data={[1, 1, 1, 1, 1, 1]}
+        keyExtractor={_keyExtractor}
+        renderItem={_renderItem}
+        ItemSeparatorComponent={_renderSeparator}
+        showsVerticalScrollIndicator={false}
+      />
+    </Container>
+  );
 };
 
 export default NotificationScreen;
